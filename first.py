b12277b86325b640a8a5eb41b22195a90dc9640f
nidhi <nidhi@nidhis-MacBook-Air.local>
2019-05-01 19:51:32 +0530

    this is first updte

---
+++ first.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3.6
+#first_name = "Navneet"
+#Last_name = "Bairwal"
+#age = 30
+#birth_date = "08_09_1989"
+#
+#print(f"My name is: {first_name} {Last_name}")
+#print(f"And my age is {age}")
+#print(f"born on {birth_date}")
+#
+## Exercise2
+
+users = [{'admin': True, 'active': True, 'name': 'Navneet' },
+         {'admin': False, 'active': True, 'name': 'Nidhi' },
+         {'admin': True, 'active': True, 'name': 'Lalit' }]
+
+line = 1
+
+for user in users:
+    prefix = f"{line} "
+
+    if user['admin'] and user['active']:
+        prefix += "ACTIVE - (ADMIN) "
+    elif user['admin']:
+        prefix += "(ADMIN) "
+    elif user['active']:
+        prefix += "ACTIVE - "
+
+    print(prefix + user['name'])
+    line += 1
